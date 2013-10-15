75218f8afe01e44a3280ee85e9bdd9d0434e8b25
Jason R. Clark <jclark@newrelic.com>
2013-10-15 13:24:21 -0700

    More repetitions, output from -Xprofile

--- go.rb
+++ go.rb
@@ -6,7 +6,9 @@ require 'benchmark'
 payload = JSON.load(File.open("./payload.json", "r"))
 
 Benchmark.bm do |x|
-  x.report { JSON.dump(payload) }
-  x.report { JSON.dump(payload) }
-  x.report { JSON.dump(payload) }
+  x.report do
+    20.times do
+      JSON.dump(payload)
+    end
+  end
 end
